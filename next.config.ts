24c22b1db25460005b324079d3978eeb705d21cd
rootz491 <karansh491@gmail.com>
2025-10-09 02:59:01 +0530

    feat: update image configuration to use remotePatterns for Sanity CDN

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
